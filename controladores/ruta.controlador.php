48c08cbfe69c024cb123a4f814d59f29313fa897
Francisco Marcial Espinoza Morales <74979974+PanchoMarcial81@users.noreply.github.com>
2022-05-24 11:46:34 -0400

    Update ruta.controlador.php

--- controladores/ruta.controlador.php
+++ controladores/ruta.controlador.php
@@ -2,6 +2,7 @@
 
 class ControladorRuta{
 	static public function ctrRuta(){
-		return "http://localhost/Proyecto-Suscripcion/";
+		//return "http://localhost/Proyecto-Suscripcion/";
+		return "https://panchomarcial81.github.io/Proyecto-Suscripcion/";
 	}
 }
